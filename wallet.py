669b7d7f62caea86393af5aa4bbb4d3137cf3857
mubeen002 <124433787+mubeen002@users.noreply.github.com>
2023-02-04 15:13:58 +0500

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,1 +1,12 @@
-
+class Wallet:
+  def __init__(self):
+    self.balance = 0
+  
+  def setAmount(self, balance):
+    self.balance = balance
+  
+  def getAmount(self):
+    return self.balance
+  
+  def removeAmount(self):
+    self.balance = 0
